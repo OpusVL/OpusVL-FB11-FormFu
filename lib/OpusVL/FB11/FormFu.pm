2f9aeeea58a17ab9971a8f006edb1b8fd0af50f3
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2015-11-19 14:27:10 +0000

    Fix dzil build

--- lib/OpusVL/FB11/FormFu.pm
+++ lib/OpusVL/FB11/FormFu.pm
@@ -2,4 +2,14 @@ use strict;
 use warnings;
 package OpusVL::FB11::FormFu;
 
+our $VERSION = '0.01';
+
 1;
+
+=head1 NAME
+
+OpusVL::FB11::FormFu - Empty module for distribution purposes
+
+=head1 DESCRIPTION
+
+See L<OpusVL::FB11::FormFu::RoleFor::Controller> and L<OpusVL::FB11::FormFu::Action::FB11FormFu>

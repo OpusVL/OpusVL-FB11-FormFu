b26d69f6695a5c5be02a0bcd4766af4592b0b73a
Colin Newell <colin@opusvl.com>
2016-07-01 15:54:14 +0100

    Added a little more explanation to the POD

--- lib/OpusVL/FB11/FormFu.pm
+++ lib/OpusVL/FB11/FormFu.pm
@@ -8,8 +8,13 @@ our $VERSION = '0.01';
 
 =head1 NAME
 
-OpusVL::FB11::FormFu - Empty module for distribution purposes
+OpusVL::FB11::FormFu - FormFu compatibility roles.
 
 =head1 DESCRIPTION
+
+This provides the role required to support HTML::FormFu in OpusVL::FB11.
+
+This is intended to allow older modules to be converted more simply over to FB11
+without a complete rewrite.
 
 See L<OpusVL::FB11::FormFu::RoleFor::Controller> and L<OpusVL::FB11::FormFu::Action::FB11FormFu>
